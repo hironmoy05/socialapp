30e74ee874eedd8ce7a9cc71bf868e433fe3fa32
Hironmoy Dhar <hironmoydhar@gmail.com>
2023-03-07 10:21:35 -0800

    My 1st production release

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const format = require('date-format');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world');
+})
+
+app.get('/api/v1/instagram', (req, res) => {
+    const instaSocial = {
+        username: 'HironmoydharOfficial',
+        followers: 66,
+        follows: 70,
+        date: format.asString('dd[MM] - hh:mm:ss', new Date()),
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+app.get('/api/v1/facebook', (req, res) => {
+    const faceSocial = {
+        username: 'HironmoydharPage',
+        followers: 88,
+        follows: 10,
+        date: format.asString('dd[MM] - hh:mm:ss', new Date()),
+    };
+
+    res.status(200).json(faceSocial);
+});
+
+app.get('/api/v1/linkedin', (req, res) => {
+    const linkedSocial = {
+        username: 'hironmoydhar',
+        followers: 80008,
+        follows: 20,
+        date: format.asString('dd[MM] - hh:mm:ss', new Date()),
+    };
+
+    res.status(200).json(linkedSocial);
+});
+
+app.get('/api/v1/:token', (req, res) => {
+    const param = req.params.token;
+    res.status(200).json({ param })
+})
+
+app.listen(PORT, () => {
+    console.log(`Running at port ${PORT}...`)
+})
